58c9e4c009878082fe1a364293429790da0fd165
sandervanvugt <mail@sandervanvugt.nl>
2023-12-07 09:06:00 -0800

    message

--- labs/lab2-grade.sh
+++ labs/lab2-grade.sh
@@ -11,7 +11,7 @@ if docker ps | grep "lab2" >/dev/null
 then 
 	echo -e "\033[32m[OK]\033[0m\t\t Container lab2 is running"
 else 
-	echo -e "\033[31m[FAIL]\033[0m\t\t I cannot find a container with the name lab2. Did you use \033[1mdocker run lab2\033[0m with the appropriate options to run it?" && exit 4
+	echo -e "\033[31m[FAIL]\033[0m\t\t I cannot find a container with the name lab2. Did you use \033[1mdocker run -d lab2\033[0m with the appropriate options to run it?" && exit 4
 fi
 
 if ss -tunap | grep 8081 >/dev/null
@@ -19,7 +19,7 @@ then
 	echo -e "\033[32m[OK]\033[0m\t\t Port 8081 on localhost is listening"
 	echo
 else
-	echo -e "\033[31m[FAIL]\033[om\t\t Nothing listening on localhost port 8081. You should probably delete the container and run it again, using \033[1mdocker stop lab2; docker rm lab2; docker run -p 8081:80 --name lab2 lab2" && exit 2
+	echo -e "\033[31m[FAIL]\033[0m\t\t Nothing listening on localhost port 8081. You should probably delete the container and run it again, using \033[1mdocker stop lab2; docker rm lab2; docker run -d -p 8081:80 --name lab2 lab2" && exit 2
 fi
 
 echo
