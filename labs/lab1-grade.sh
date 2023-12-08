35907411f4653ace51cfe237427e833f61cf3ab1
sandervanvugt <mail@sandervanvugt.nl>
2023-12-08 08:24:52 -0800

    message

--- labs/lab1-grade.sh
+++ labs/lab1-grade.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if ps aux | grep minikube >/dev/null
+if ps aux | grep docker >/dev/null
 then 
-	echo -e "\033[32m[OK]\033[0m\t\t Good! Minikube is running"
+	echo -e "\033[32m[OK]\033[0m\t\t Good! docker is running"
 else 
-	echo -e "\033[31m[FAIL]\033[0m\t\t I don't see minikube running. Please use \033[1mgit clone https://github.com/sandervanvugt/kubestep\033[0m and run \033[1m./minikube-docker-setup.sh \033[0m from the course Git repository to start it." && exit 3
+	echo -e "\033[31m[FAIL]\033[0m\t\t I don't see docker running. Please use \033[1mgit clone https://github.com/sandervanvugt/kubestep\033[0m and run \033[1m./minikube-docker-setup.sh \033[0m from the course Git repository to start it." && exit 3
 fi
 
 if docker ps | grep "nginx" >/dev/null
